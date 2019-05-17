03f03ca0ce450b313b38aa318790afc07a81ef0d
Joao Paulo Furtado Silva <joaopaulofurtado@live.com>
2019-05-17 10:37:33 -0700

    commented out unused code

--- src/components/pages/Base/Header/UserMenu.js
+++ src/components/pages/Base/Header/UserMenu.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
 
-import {Link, NavLink} from "react-router-dom";
+// import {Link, NavLink} from "react-router-dom";
 import {userLogout} from "../../../../actions/authActions";
-import history from './../../../../history';
+// import history from './../../../../history';
 
 class UserMenu extends Component{
     render(){
         return(
                 <div className="ui card">
-                    { this.props.isLoggedIn && 
+                    { this.props.isLoggedIn &&
                             <div className="content" onClick={()=> this.props.userLogout()}>
                                 <p>Logout</p>
                             </div>
