d8fa1cfe5e32eb73d02271f061467583b9c4d591
Joao Paulo Furtado Silva <joaopaulofurtado@live.com>
2019-05-22 13:54:42 -0700

    landing basic copy

--- src/components/pages/Landing/Landing.js
+++ src/components/pages/Landing/Landing.js
@@ -36,41 +36,54 @@ class Landing extends Component {
                             <div className="row">
                                 <div className="sixteen wide column">
                                     <h3 className="ui center aligned header text-hackachieve-dark-purple ">
-                                        YOUR GOALS AT YOUR FEET, YOUR HAND ACTUALLY</h3>
-                                    <p className="text-grey">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                                        sed diam nonumy
-                                        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-                                        voluptua. At vero
-                                        eos et accusam et justo duo dolores et ea rebum.</p>
+                                        TAKE ABSOLUTE CONTROL OF YOUR LIFE</h3>
+                                    <p className="text-grey">
+
+                                        Are you tired of procrastinating your future and not having any clue of where
+                                        are you going to in your life?
+                                        <br/>
+                                        Hackachieve helps you connecting the dots by setting a productivity framework
+                                        for you! You'll be able to set both <strong>long term</strong> and <strong>short
+                                        term</strong> goals, prioritize by impact and categorize everything into an
+                                        organized environment!
+                                        <br/><br/>
+                                        What are you waiting for? <br/><br/> <NavLink to="/register">Create your free
+                                        account</NavLink> right now and enjoy our exclusive methodology!</p>
                                 </div>
                             </div>
 
                             <div className="row">
                                 <div className="eight wide column mar-top-60 head-left">
-                                    <div className="b-img"><img src="images/banana.png" alt=""/></div>
-                                    <h3 className="ui aligned header text-hackachieve-dark-purple "> WE STILL MAKE
-                                        BANANAS THAT CAN
-                                        DANCE</h3>
-                                    <p className="text-grey">Banana ipsum dolor sit amet, consetetur sadipscing elitr,
-                                        sed diam
-                                        nonumy<br/> banana tempor invidunt ut labore et dolore magna banana erat, sed
-                                        diam<br/>
-                                        voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-                                        Banana.</p>
+                                    <div className="b-img">
+                                        <i className="far fa-star landing-icon"></i></div>
+                                    <h3 className="ui aligned header text-hackachieve-dark-purple "> PRIORITIZE WHAT
+                                        MATTERS</h3>
+                                    <p className="text-grey">
+
+                                        Its very common to lose yourself trying to plan a pathway to a particular long
+                                        term goal. Prioritizing helps you to define what's more important to the present
+                                        moment in your life, so you can accomplish your short term goals one step at a
+                                        time.</p>
                                 </div>
                             </div>
 
                             <div className="row row-reverse">
                                 <div className="eight wide column mar-top-60 head-right">
-                                    <div className="b-img"><img src="images/banana.png" alt=""/></div>
-                                    <h3 className="ui aligned header text-hackachieve-dark-purple "> MORE BANANAS CAME
-                                        TO THE PARTY</h3>
-                                    <p className="text-grey">Banana ipsum dolor sit amet, consetetur sadipscing elitr,
-                                        sed diam
-                                        nonumy<br/> banana tempor invidunt ut labore et dolore magna banana erat, sed
-                                        diam<br/>
-                                        voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-                                        Banana.</p>
+                                    <div className="b-img">
+                                        <i className="fas fa-columns landing-icon"></i>
+                                    </div>
+                                    <h3 className="ui aligned header text-hackachieve-dark-purple "> ORGANIZE
+                                        YOURSELF</h3>
+                                    <p className="text-grey">
+
+                                        In almost every productivity app you end up getting lost after some time, since
+                                        there's a ton of things to do without any contextual organization.
+
+                                        <br/>
+                                        In Hackachieve, we split your long term goals in different categories (health,
+                                        finances, etc) and each one of them will have its related cards. You can also
+                                        filter out categories from your board view
+                                    </p>
                                 </div>
 
 
