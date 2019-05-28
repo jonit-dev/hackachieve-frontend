6d96ea8a7d3b93c77d01722c0cf24242db7ea13b
Joao Paulo Furtado Silva <joaopaulofurtado@live.com>
2019-05-27 23:32:01 -0700

    /preferences page implemented

--- src/components/pages/Landing/Preferences/Preferences.js
+++ src/components/pages/Landing/Preferences/Preferences.js
@@ -24,37 +24,46 @@ class Preferences extends Component {
             <div className="home_header login_header">
                 <a className="mobile-logo" href=" #"><img src="/images/board/hackachieve-symbol.svg" alt="Logo"/></a>
                 <div className="login-sec">
-                    <h3>Preferences</h3>
-                    <div className="login-inner">
 
 
-                        <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="login-form">
-                            <Field name="title" component={this.renderInput} label="Enter title"/>
-                            <Field name="description" component={this.renderInput} label="Enter description"/>
+                    <div className="form-wrapper">
 
-                            <button id="btnSignupForm" className="button">Submit</button>
+                        <h3>Preferences</h3>
 
-
-                        </form>
+                        <div className="login-inner">
 
 
-                        {/*<hr></hr>*/}
-
-                        {/*<h5 className="padding">You don’t need to decorate new passwords if you don’t want to,</h5>*/}
-
-                        {/*<div className="login-bottom">*/}
-
-                        {/*<div className="f-img"><a href="#"><img src="/images/login-facebook.svg" alt="Logo"*/}
-                        {/*className="f-logo">*/}
-                        {/*</a>*/}
-                        {/*</div>*/}
-                        {/*<div className="g-img">*/}
-                        {/*<a href="#"><img src="/images/login-google.svg" alt="Logo" className="g-logo"></a>*/}
-                        {/*</div>*/}
-                        {/*</div>*/}
 
 
+                            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="login-form">
+                                <Field name="title" component={this.renderInput} label="Enter title"/>
+                                <Field name="description" component={this.renderInput} label="Enter description"/>
+
+                                <button id="btnSignupForm" className="button">Submit</button>
+
+
+                            </form>
+
+
+                            {/*<hr></hr>*/}
+
+                            {/*<h5 className="padding">You don’t need to decorate new passwords if you don’t want to,</h5>*/}
+
+                            {/*<div className="login-bottom">*/}
+
+                            {/*<div className="f-img"><a href="#"><img src="/images/login-facebook.svg" alt="Logo"*/}
+                            {/*className="f-logo">*/}
+                            {/*</a>*/}
+                            {/*</div>*/}
+                            {/*<div className="g-img">*/}
+                            {/*<a href="#"><img src="/images/login-google.svg" alt="Logo" className="g-logo"></a>*/}
+                            {/*</div>*/}
+                            {/*</div>*/}
+
+
+                        </div>
                     </div>
+
                 </div>
                 <div className="clear"></div>
             </div>
