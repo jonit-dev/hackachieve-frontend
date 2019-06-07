9c0a4ff2f6fc4bb431b8663e97834dfde517376d
Joao Paulo Furtado <joaopaulofurtado@live.com>
2019-06-06 19:25:59 -0700

    hook

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-//Redux specific
+//Redux specificc
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose} from 'redux';
 import reducers from './reducers';
