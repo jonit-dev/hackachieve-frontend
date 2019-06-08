e6b3fecc522c15bc7128a74dc98912abc609883f
Joao Paulo Furtado <joaopaulofurtado@live.com>
2019-06-08 12:47:05 -0700

    removed alert message in route error

--- src/classes/API.js
+++ src/classes/API.js
@@ -24,7 +24,7 @@ export default class API {
             catch (error) { //if something wrong happens, lets just redirect the user to login and show an error message
                 // console.log(error);
                 // history.push({pathname: '/login', state: {alert: 'Please, login before accessing this page'}})
-                history.push({pathname: '/', state: {alert: 'Please, login before accessing this page'}})
+                history.push({pathname: '/'})
             }
         }
 
