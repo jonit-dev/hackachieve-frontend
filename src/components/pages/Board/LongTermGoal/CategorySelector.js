51b49ced7ff51c13dbe607a095db6e13fd9a7cbb
Joao Paulo Furtado <joaopaulofurtado@live.com>
2019-06-22 09:40:08 -0700

    fixes on board colors, etc.

--- src/components/pages/Board/LongTermGoal/CategorySelector.js
+++ src/components/pages/Board/LongTermGoal/CategorySelector.js
@@ -5,19 +5,23 @@ import CreatableSelect from "react-select/creatable/dist/react-select.esm";
 export const CategorySelector = props => {
     console.log('props', props);
     const {input, options, label} = props;
-console.log(input,options,'find')
+    console.log(input, options, 'find')
     return (
         <>
-        <strong><label>{label}</label></strong>
-        <CreatableSelect
-            isClearable
-            options={options}
-            // onBlur={() => input.onBlur(input.value)}
-            onChange={(value, actions) => {
-                input.onChange({...value, ...actions})
-            }}
-            onBlur={() => input.onBlur(input.value)}
-        />
+            <div className="field">
+
+                <label>{label}</label>
+                <CreatableSelect
+                    isClearable
+                    options={options}
+                    // onBlur={() => input.onBlur(input.value)}
+                    onChange={(value, actions) => {
+                        input.onChange({...value, ...actions})
+                    }}
+                    onBlur={() => input.onBlur(input.value)}
+                />
+
+            </div>
         </>
     )
 };
