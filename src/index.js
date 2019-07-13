d28dacdabf664a4dab3f0eee4ae821425edf4ccb
Joao Paulo Furtado <joaopaulofurtado@live.com>
2019-07-13 13:53:44 -0700

    added open graph metatags

--- src/index.js
+++ src/index.js
@@ -21,11 +21,29 @@ ReactDOM.render(
     <Provider store={store}>
       <Helmet>
         <meta charSet="utf-8"></meta>
-        <title>Hackachieve - Double your productivity!</title>
+        <title>Hackachieve - Use smart goals to double your productivity!</title>
         <link rel="canonical" href="https://www.hackachieve.com" />
         <meta name="description" content="Use Hackachieve to apply smart goals in your business or personal life"/>
         <meta name="keywords" content="smart goals, productivity, kanban, scrum, personal productivity, business productivity"/>
         <meta name="language" content="English"/>
+ 
+      <meta itemprop="name" content="Hackachieve - Use smart goals to double your productivity!"/>
+      <meta itemprop="description" content="se Hackachieve to apply smart goals in your business or personal life"/>
+      <meta itemprop="image" content="https://www.hackachieve.com/landing_resources/images/dashboard.png"/>
+ 
+      <meta name="twitter:card" content="summary"/>
+      <meta name="twitter:title" content="Hackachieve - Use smart goals to double your productivity!"/>
+      <meta name="twitter:description" content="se Hackachieve to apply smart goals in your business or personal life"/>
+      
+      <meta name="og:title" content="Hackachieve - Use smart goals to double your productivity!"/>
+      <meta name="og:description" content="se Hackachieve to apply smart goals in your business or personal life"/>
+      <meta name="og:image" content="https://www.hackachieve.com/landing_resources/images/landing_1.png"/>
+      <meta name="og:url" content="https://www.hackachieve.com"/>
+      <meta name="og:site_name" content="Hackachieve"/>
+      <meta name="og:locale" content="en_US"/>
+      <meta name="og:type" content="website"/>
+
+
       </Helmet>
         <App/>
     </Provider>,
