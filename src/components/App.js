d9f688c93588986607e908e9d91b54f0e7d88e9a
Joao Paulo Furtado <joaopaulofurtado@live.com>
2019-07-13 11:10:06 -0700

    added scrollToTop on react dom router

--- src/components/App.js
+++ src/components/App.js
@@ -1,80 +1,74 @@
-import React, {Component} from 'react';
-import history from './../history';
-import {connect} from 'react-redux'
-import {Route, Router, Switch} from "react-router-dom";
-import './App.scss';
-import 'simplebar'; // or "import SimpleBar from 'simplebar';" if you want to use it manually.
-import 'simplebar/dist/simplebar.css';
+import React, { Component } from "react";
+import history from "./../history";
+import { connect } from "react-redux";
+import { Route, Router, Switch } from "react-router-dom";
+import "./App.scss";
+import "simplebar"; // or "import SimpleBar from 'simplebar';" if you want to use it manually.
+import "simplebar/dist/simplebar.css";
 
 /* Pages =========================================== */
-import Landing from './pages/Landing/Landing';
+import Landing from "./pages/Landing/Landing";
 import Header from "./pages/Base/Header/Header";
 import Login from "./pages/Landing/Login/Login";
 import Register from "./pages/Landing/Register/Register";
-import {checkLoggedIn} from "../actions/authActions";
+import { checkLoggedIn } from "../actions/authActions";
 import Board from "./pages/Board/Board";
-import env from '../env';
+import env from "../env";
 import TermsOfUse from "./pages/Landing/Legal/TermsOfUse";
 import PrivacyPolicy from "./pages/Landing/Legal/PrivacyPolicy";
 import Footer from "./pages/Base/Footer/Footer";
 import Preferences from "./pages/Landing/Preferences/Preferences";
 import FullStory from "react-fullstory";
-import NotFound from './pages/Base/NotFound/NotFound';
+import NotFound from "./pages/Base/NotFound/NotFound";
+import ScrollToTop from './pages/Base/Routing/ScrollToTop';
 
-let isProd = env.env === 'prod';
-
+let isProd = env.env === "prod";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.checkLoggedIn(); //check logged in on app init
 
+    console.log(`Initializing app. Environment is ${env.env}`);
 
+    //Necessary for GTM
+    window.dataLayer = window.dataLayer || [];
+  }
 
-    componentDidMount() {
+  render() {
+    return (
+      <Router history={history}>
+        <ScrollToTop>
+          {isProd ? <FullStory org="MJK6R" /> : null}
+          <Header />
 
-        this.props.checkLoggedIn(); //check logged in on app init
-
-        console.log(`Initializing app. Environment is ${env.env}`);
-
-        //Necessary for GTM
-        window.dataLayer = window.dataLayer || [];
-
-    }
-
-    render() {
-        return (
-            <Router history={history}>
-                {(isProd ? <FullStory org="MJK6R"/> : null)}
-                <Header/>
-
-                <Switch>
-
-                    <Route path="/" exact component={Landing}/>
-                    <Route path="/login" component={Login}/>
-                    <Route path="/register" component={Register}/>
-                    <Route path="/terms" component={TermsOfUse}/>
-                    <Route path="/privacy" component={PrivacyPolicy}/>
-                    <Route path="/board" component={Board}/>
-                    <Route path="/preferences" component={Preferences}/>
-                    <Route path="*" component={NotFound}/>
-
-                </Switch>
-                <Footer/>
-            </Router>
-        );
-    }
+          <Switch>
+            <Route path="/" exact component={Landing} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/terms" component={TermsOfUse} />
+            <Route path="/privacy" component={PrivacyPolicy} />
+            <Route path="/board" component={Board} />
+            <Route path="/preferences" component={Preferences} />
+            <Route component={NotFound} />
+          </Switch>
+          <Footer />
+        </ScrollToTop>
+      </Router>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-
-        token: state.auth.token,
-        isLoggedIn: state.auth.isLoggedIn
-    };
+const mapStateToProps = state => {
+  return {
+    token: state.auth.token,
+    isLoggedIn: state.auth.isLoggedIn
+  };
 };
 
-export default connect(mapStateToProps, {
+export default connect(
+  mapStateToProps,
+  {
     //actions here
     checkLoggedIn
-
-
-})(App);
-
+  }
+)(App);
