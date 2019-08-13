1314e4d7118b3521464f58354f7a858953d4a0a8
Joao Paulo Furtado <joaopaulofurtado@live.com>
2019-08-12 22:53:53 -0700

    social icons to landing page

--- src/components/pages/Base/Footer/Footer.js
+++ src/components/pages/Base/Footer/Footer.js
@@ -1,40 +1,60 @@
-import React from 'react';
-import {Link, withRouter} from "react-router-dom";
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
 
-const Footer = (props) => {
+const Footer = props => {
+  switch (props.location.pathname) {
+    // case '/board':
+    //
+    //     return null;
 
-    switch (props.location.pathname) {
+    default:
+      return (
+        <React.Fragment>
+          <div className="footer py-5 border-top text-center">
+            <div className="container">
+              <div className="row">
+                <div className="col-12">
+                  <ul className="social-icons">
+                    <li>
+                      <a
+                        href="https://www.facebook.com/hackachieve/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <i className="fab fa-facebook"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.instagram.com/hackachieve.oficial/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <i className="fab fa-instagram"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
 
-        // case '/board':
-        //
-        //     return null;
+              <div className="row">
+                <div className="col-12">
+                  <p className="mb-0">
+                    <Link to="/terms" className="p-3 footer-item">
+                      Terms Of Use
+                    </Link>
 
-
-        default:
-            return (
-                <React.Fragment>
-                    <div className="footer py-5 border-top text-center">
-          <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <p className="mb-0">
-                  <Link to="/terms" className="p-3 footer-item">Terms Of Use</Link>
-                  
-                  <Link to="/privacy" className="p-3 footer-item">Privacy Policy</Link>
-                  
-                </p>
+                    <Link to="/privacy" className="p-3 footer-item">
+                      Privacy Policy
+                    </Link>
+                  </p>
+                </div>
               </div>
             </div>
-            
           </div>
-        </div>
-                </React.Fragment>
-            )
-
-
-    }
-
-
+        </React.Fragment>
+      );
+  }
 };
 
 export default withRouter(Footer);
